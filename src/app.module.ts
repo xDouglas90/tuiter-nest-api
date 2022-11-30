c8cef13201605d58f0c9b249b23f7fd478cffb95
odouglas.dev <odouglas.dev@gmail.com>
2022-11-29 21:13:05 -0300

    :wrench: chore: Config typeorm with postgresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TuitsModule } from './tuits/tuits.module';
 import { UsersModule } from './users/users.module';
+import { config } from 'dotenv';
+
+config();
 
 @Module({
-  imports: [TuitsModule, UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USERNAME,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    TuitsModule,
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
